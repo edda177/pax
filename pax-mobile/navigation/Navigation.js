5090252802f868dc47741163c98b2a5e0fb36b4f
Hannakind <Hanna.kindholm@chasacademy.se>
2025-04-09 12:09:29 +0200

    Start native

---
+++ pax-mobile/navigation/Navigation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import Booking from "../screens/Booking";
+
+const Tab = createBottomTabNavigator();
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                />
+
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Navigation;
---
+++ pax-mobile/screens/HomeScreen.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+const HomeScreen = () => {
+  return <ScrollView></ScrollView>;
+};
+
+export default HomeScreen;
