b677cb38360ac0a2a19bf814a5fc7cd84c2c850a
Hannakind <Hanna.kindholm@chasacademy.se>
2025-04-17 10:05:41 +0200

    del calender

--- pax-mobile/screens/HomeScreen.jsx
+++ pax-mobile/screens/HomeScreen.jsx
@@ -1,41 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Calendar } from "react-native-calendars";
 
 const HomeScreen = () => {
-  const onDayPress = (day) => {
-    console.log("Valt datum:", day.dateString);
-  };
-
   return (
-    <View style={styles.container}>
-      <Calendar
-        onDayPress={onDayPress}
-        style={styles.calendar}
-        theme={{
-          selectedDayBackgroundColor: "#00adf5",
-          todayTextColor: "#00adf5",
-          arrowColor: "#00adf5",
-          textMonthFontSize: 20,
-        }}
-      />
+    <View>
+      <Text>HomeScreen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-    backgroundColor: "#f9f9f9",
-  },
-  calendar: {
-    borderRadius: 10,
-    elevation: 4,
-    backgroundColor: "white",
-    paddingBottom: 10,
-  },
-});
-
 export default HomeScreen;
+
+const styles = StyleSheet.create({});
