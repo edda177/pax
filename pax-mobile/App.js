c1870a7108b2df8b9163c85cbe2eb5e420ee0907
tovaalicia <tovahansens@gmail.com>
2025-04-23 10:46:27 +0200

    themeContext

--- pax-mobile/App.js
+++ pax-mobile/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from './Navigation/BottomTabNavigator';
+import { ThemeProvider } from 'theme/ThemeContext';
 
 export default function App() {
   return (
