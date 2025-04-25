67ee7a3f0799932addaafd2f93afca0603005337
Dennis Granheimer <dennis.granheimer@live.se>
2025-04-25 11:44:53 +0200

    added simple users page

--- dashboard/src/pages/Users.tsx
+++ dashboard/src/pages/Users.tsx
@@ -1,35 +1,75 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/Card";
 import { User } from "../types/user";
+import Button from "@/components/Button";
 const Users: React.FC = () => {
   const mockUsers: User[] = [
     {
       id: 1,
-      username: "alice",
+      name: "Alice",
+      surname: "Andersson",
+      email: "alice@example.com",
       password: "password123",
       role: "admin",
     },
     {
       id: 2,
-      username: "bob",
+      name: "Bob",
+      surname: "Berg",
+      email: "bob@example.com",
       password: "securepass",
       role: "user",
     },
     {
       id: 3,
-      username: "charlie",
+      name: "Charlie",
+      surname: "Carlsson",
+      email: "charlie@example.com",
       password: "charliepw",
       role: "moderator",
     },
     {
       id: 4,
-      username: "diana",
+      name: "Diana",
+      surname: "Dahl",
+      email: "diana@example.com",
       password: "dianapw",
       role: "user",
     },
   ];
+  console.log(mockUsers);
   return (
-    <div>
-      <h1>Users</h1>
-    </div>
+    <main className="flex flex-col justify-center p-6 bg-[#10302B] gap-4 text-white w-full h-screen overflow-auto">
+      <section className="flex gap-4">
+        <h1 className="text-2xl font-bold text-white">Users</h1>
+        <Button onClick={() => {}}>Add User</Button>
+      </section>
+      <section className="flex-grow gap-4 flex">
+        {mockUsers.map((user) => (
+          <Card key={user.id} className="h-fit">
+            <CardHeader>
+              <CardTitle>
+                {user.name} {user.surname}
+              </CardTitle>
+              <CardDescription>{user.email}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p> Role:{user.role}</p>
+            </CardContent>
+            <CardFooter className="flex gap-2">
+              <Button onClick={() => {}}>Update</Button>
+              <Button onClick={() => {}}>Delete</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </section>
+    </main>
   );
 };
 
--- dashboard/src/types/user.ts
+++ dashboard/src/types/user.ts
@@ -1,6 +1,8 @@
 export type User = {
   id: number;
-  username: string;
+  name: string;
+  surname: string;
+  email: string;
   password: string;
   role: string;
 };
