381cb3c0359dc0d7e23ad3af71adab0f6f1f5922
Johan Modin <carljohan.modin@gmail.com>
2025-05-13 14:13:15 +0200

    Add temp sensor class for extracting temperature and humidity value from a DHT 11 sensor. Co-authored-by: NewNamesAreHard, bubba-94, binasime

--- include/temp_sensor.h
+++ include/temp_sensor.h
@@ -1,12 +1,6 @@
-#ifndef TEMP_SENSOR
-#define TEMP_SENSOR
-#include <arduino.h>
-
-//! uint8_t defined in lib below
-#include <cstdint>
-
-// std::byte defined in lib below
-#include <cstddef>
+#ifndef TEMP_SENSOR_H
+#define TEMP_SENSOR_H
+#include <Arduino.h>
 
 /**
  * @class Class for temperature and humidity sensor DHT11
@@ -14,30 +8,66 @@
  * Values read will be formatted into json string and sent via Ether/WiFi.
  */
 class TempSensor
-{
-private:
-    //! Array to store data and read from
-    std::byte data[5];
-    //! Pin number of DHT 11 sensor.
-    uint8_t m_pin;
-
+{  
 public:
-    //! Constructor for temp sensor.
-    TempSensor(uint8_t pin) {}
+    /**
+    * @brief Initialize sensor
+    */
+    void begin();
 
     /**
-     * @brief
-     * @param pin Pin I/O to read from
-     * @details Method will store data in byte array
-     * @return
-     */
-    std::byte read(uint8_t pin);
+    * @brief Get the Temperature object
+    * @return float value of temperature (%)
+    */
+    float get_temperature();
+
+    /**
+    * @details We have not talked about using humidity values yet
+    * @return float value of humidity (%)
+    */
+    float get_humidity();
 
     /**
      * @brief A method for getting the current time passed (ms)
      * @return millis()
      */
     unsigned long current_time();
+
+    //! Constructor for temp sensor.
+    TempSensor(uint8_t pin) {}
+
+private:
+    /**
+     * @brief Populate byte array 
+     */
+    void store_sensor();
+    
+    /**
+     * @brief Method will store data in byte array
+     * @return Arduino define byte array
+     */
+    byte read_sensor();
+
+    //! Store last polled time with millis()
+    unsigned long last_polled;
+    //! Array to store data and read from
+    byte data[5];
+    //! Pin number of DHT 11 sensor.
+    uint8_t pin;
 };
+
+/*
+Example Usage:
+    TempSensor DHT11(PIN_NUMBER);
+
+    void setup()
+    {
+        DHT11.begin();
+    }
+    void loop(
+    {
+        DHT11.get_temperature();
+    }
+*/
 
 #endif // TEMP_SENSOR
--- src/temp_sensor.cpp
+++ src/temp_sensor.cpp
@@ -1,64 +1,88 @@
 #include "../include/temp_sensor.h"
 
-TempSensor::TempSensor(uint8_t pin) : m_pin{pin} {}
+TempSensor::TempSensor(uint8_t pin) : pin{pin} {}
 
 unsigned long TempSensor::current_time()
 {
     return millis();
 }
 
-std::byte TempSensor::read(uint8_t pin)
+//! To prevent zero values 
+void TempSensor::begin()
 {
+    // Set pin to OUTPUT before entering store_sensor
+    pinMode(pin, OUTPUT);
+    store_sensor();
+}
+
+byte TempSensor::read_sensor()
+{
+    byte data;
     for (int i = 0; i < 8; i++)
     {
         if (digitalRead(pin) == LOW)
         {
             while (digitalRead(pin) == LOW)
-                ;                  // wait 50us;
+            {
+            // wait 50us; 
+            }   
             delayMicroseconds(30); // Duration of high level determine whether data is 0 or 1
             if (digitalRead(pin) == HIGH)
+            {
                 data |= (1 << (7 - i)); // High in the former, low in the post;
+            }
             while (digitalRead(pin) == HIGH)
-                ; // Data '1', waiting for next bit
+            {
+            // Data '1', waiting for next bit
+            } 
         }
     }
     return data;
 }
 
-void start_test(uint8_t pin)
+void TempSensor::store_sensor()
 {
+    this->last_polled = current_time();
     digitalWrite(pin, LOW); // Pull down the bus to send the start signal;
     delay(30);              // The delay is greater than 18 ms so that DHT 11 can detect the start signal;
     digitalWrite(pin, HIGH);
     delayMicroseconds(40); // Wait for DHT11 to respond;
+    pinMode(pin, INPUT);
     while (digitalRead(pin) == HIGH)
-        ;
+    {
+    }
     delayMicroseconds(80); // The DHT11 responds by pulling the bus low for 80us;
     if (digitalRead(pin) == LOW)
-        ;
+    {
+    }
     delayMicroseconds(80);      // DHT11 pulled up after the bus 80us to start sending data;
     for (int i = 0; i < 4; i++) // Receiving data, check bits are not considered;
-        data[i] = read();
+    {
+        this->data[i] = read_sensor();
+    }
     pinMode(pin, OUTPUT);
     digitalWrite(pin, HIGH); // After release of bus, wait for host to start next signal
 }
-void setup()
-{
-    Serial.begin(9600);
-    pinMode(DHpin, OUTPUT);
+
+//TempSensor
+float TempSensor::get_temperature()
+{   
+    // This is to ensure we do not poll too often compared to what store_sensor() can handle
+    if (current_time() >= this->last_polled + 1000) 
+    {
+        store_sensor();
+    }
+    // data[2] integer value of temp. data[3] decimal value of temp 
+    return float(data[2]) + (float(data[3]) / 10); 
 }
-void loop()
-{
-    start_test();
-    Serial.print("Current humdity = ");
-    Serial.print(data[0], DEC); // Displays the integer bits of humidity;
-    Serial.print('.');
-    Serial.print(data[1], DEC); // Displays the decimal places of the humidity;
-    Serial.println('%');
-    Serial.print("Current temperature = ");
-    Serial.print(data[2], DEC); // Displays the integer bits of temperature;
-    Serial.print('.');
-    Serial.print(data[3], DEC); // Displays the decimal places of the temperature;
-    Serial.println('C');
-    delay(700);
+
+float TempSensor::get_humidity()
+{   
+    // This is to ensure we do not poll too often compared to what store_sensor() can handle
+    if (current_time() >= this->last_polled + 1000)
+    {
+        store_sensor();
+    }
+    // data[0] integer value of temp. data[1] decimal value of temp 
+    return float(data[0]) + (float(data[1]) / 10); 
 }
