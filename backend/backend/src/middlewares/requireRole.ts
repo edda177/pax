29ecaf26848baf1d9e97d65d00f37202c4a89e16
Rhibro <rhibron@gmail.com>
2025-05-21 13:13:37 +0200

    fixed small bugs in middleware concerning types

--- backend/backend/src/middlewares/requireRole.ts
+++ backend/backend/src/middlewares/requireRole.ts
@@ -2,7 +2,7 @@ import { Response, NextFunction } from 'express';
 import { AuthenticatedRequest } from './authMiddleware.ts';
 
 const requireRole = (role: string) => {
-    return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    return (req: AuthenticatedRequest, res: Response, next: NextFunction): Response | undefined => {
         if (!req.user || req.user.role !== role) {
             return res.status(403).json({ message: 'Access denied: insufficient role' });
         }
