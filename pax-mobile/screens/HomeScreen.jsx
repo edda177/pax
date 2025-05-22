0af352cb9c60093d537744e9a8d372e74607e55f
Hannakind <Hanna.kindholm@chasacademy.se>
2025-05-22 10:18:41 +0200

    Avboka/pausa

--- pax-mobile/screens/HomeScreen.jsx
+++ pax-mobile/screens/HomeScreen.jsx
@@ -8,11 +8,16 @@ import {
   Platform,
   Pressable,
   Image,
+  Dimensions,
 } from "react-native";
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useTheme } from "../theme/ThemeContext";
 import CardComponent from "../components/CardComponent";
+import RoomCard from "../components/RoomCard";
+import WelcomeCard from "../components/WelcomeCard";
+
+const { width, height } = Dimensions.get("window");
 
 const HomeScreen = () => {
   const { theme, isDark, toggleTheme } = useTheme();
@@ -20,44 +25,46 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaProvider>
-      <ScrollView>
-    <SafeAreaView style={styles.safeArea}>
-              <View style={styles.header}>
-      <StatusBar
-        barStyle={isDark ? "light-content" : "dark-content"}
-        backgroundColor={theme.headerBackground}
-      />
-        <Pressable
-          onPress={toggleTheme}
-          accessible={true}
-          accessibilityRole="button"
-          accessibilityLabel={
-            isDark ? "Byt till ljust tema" : "Byt till mörkt tema"
-          }
-          style={styles.toggleButton}
-        >
-          <MaterialCommunityIcons
-            name={isDark ? "white-balance-sunny" : "weather-night"}
-            size={30}
-            color={theme.iconSwitchmode}
-          />
-        </Pressable>
-              <Image
-      source={require("../assets/logo/pax_logo-text-10.png")}
-      style={styles.logo}
-      resizeMode="cover"
-    />
+      <ScrollView style={styles.ScrollView}>
+        <SafeAreaView style={styles.safeArea}>
+          <View style={styles.header}>
+            <StatusBar
+              barStyle={isDark ? "light-content" : "dark-content"}
+              backgroundColor={theme.headerBackground}
+            />
+            <Pressable
+              onPress={toggleTheme}
+              accessible={true}
+              accessibilityRole="button"
+              accessibilityLabel={
+                isDark ? "Byt till ljust tema" : "Byt till mörkt tema"
+              }
+              style={styles.toggleButton}
+            >
+              <MaterialCommunityIcons
+                name={isDark ? "white-balance-sunny" : "weather-night"}
+                size={30}
+                color={theme.iconSwitchmode}
+              />
+            </Pressable>
+            <Image
+              source={require("../assets/logo/pax_logo-text-10.png")}
+              style={styles.logo}
+              resizeMode="contain"
+            />
           </View>
-                <View style={styles.container}>
-        <CardComponent style={styles.CardComponentContainer} />
-              <Image
-      source={require("../assets/illustrationer/illustrationer_pax-27.svg")}
-      style={styles.image}
-      resizeMode="cover"
-    />
-      </View>
-    </SafeAreaView>
-    </ScrollView>
+
+          <WelcomeCard />
+          <RoomCard />
+          <View style={styles.container}>
+            <Image
+              source={require("../assets/illustrationer/illustrationer_pax-27.svg")}
+              style={styles.image}
+              resizeMode="contain"
+            />
+          </View>
+        </SafeAreaView>
+      </ScrollView>
     </SafeAreaProvider>
   );
 };
@@ -67,53 +74,44 @@ const createStyles = (theme) =>
     safeArea: {
       flex: 1,
       backgroundColor: theme.background,
-      minHeight: "100vh",
-      paddingBottom: 2,
     },
+
+    ScrollView: {
+      backgroundColor: theme.background,
+    },
+
     header: {
-      flex: 1,
       flexDirection: "row",
       justifyContent: "center",
       alignItems: "center",
+      paddingVertical: 16,
+      position: "relative",
     },
     container: {
-      flex: 1,
-      backgroundColor: theme.background,
-      justifyContent: "flex-start",
       alignItems: "center",
-      marginTop: "10vh",
-      minHeight: "90vh",
+      marginTop: 100,
     },
     logo: {
-      top: 5,
-      marginTop: "4vh",
-      width: "40vw",
-      height: "8vh",
-      padding: 10,
+      width: width * 0.4,
+      height: 50,
+    },
+    image: {
+      width: width * 1.2,
+      height: height * 0.3,
+      opacity: 0.75,
+    },
+    toggleButton: {
+      position: "absolute",
+      top: Platform.OS === "ios" ? 0 : 10,
+      left: 20,
+      backgroundColor: "#E3FAC825",
+      borderRadius: 25,
+      padding: 5,
+      zIndex: 1,
     },
     text: {
       fontSize: 18,
       color: theme.text,
-    },
-      image: {
-    width: "90vw",
-    height: "30vh",
-    opacity: 0.75,
-  },
-    toggleButton: {
-      position: "absolute",
-      top: 20,
-      left: 20,
-      zIndex: 1,
-      backgroundColor: "#E3FAC825",
-      borderRadius: 25,
-      padding: 5,
-    },
-    CardComponentContainer: {
-      marginTop: "20%",
-      bottom: 5,
-      alignSelf: "center",
-      width: "50vw",
     },
   });
 
