0f25564a73b9c77a1ae80ccdd03329d19d384356
Hannakind <Hanna.kindholm@chasacademy.se>
2025-05-26 09:37:02 +0200

    ändring av nav

--- pax-mobile/Navigation/BottomTabNavigator.js
+++ pax-mobile/Navigation/BottomTabNavigator.js
@@ -5,12 +5,8 @@ import Icon from "react-native-vector-icons/FontAwesome5";
 import HomeScreen from "../screens/HomeScreen";
 import Booking from "../screens/Booking";
 import BookingButton from "../components/BookingButton";
-import { useTheme } from "../theme/ThemeContext";
-import LoadingScreen from "../screens/LoadingScreen";
-import Rooms from "../screens/Rooms";
 import Contact from "../screens/Contact";
-import BookedScreen from "../screens/BookedScreen";
-import LoginTest from "../screens/LoginScreen";
+import { useTheme } from "../theme/ThemeContext";
 
 const Tab = createBottomTabNavigator();
 
@@ -27,17 +23,9 @@ export default function BottomTabNavigator() {
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
-          if (route.name === "Home") iconName = focused ? "home" : "home";
-          else if (route.name === "BookedScreen")
-            iconName = focused ? "check-square" : "check-square";
-          else if (route.name === "Login")
-            iconNameHuh = focused ? "ööh?" : "ööh?";
+          if (route.name === "Home") iconName = "home";
+          else if (route.name === "Contact") iconName = "envelope";
           else if (route.name === "Booking") return null;
-          else if (route.name === "LoadingScreen")
-            iconNameHuh = focused ? "ööh?" : "ööh?";
-          else if (route.name === "Rooms") iconName = focused ? "map" : "map";
-          else if (route.name === "Contact")
-            iconName = focused ? "envelope" : "envelope";
 
           return (
             <View
@@ -65,9 +53,6 @@ export default function BottomTabNavigator() {
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="BookedScreen" component={BookedScreen} />
-      <Tab.Screen name="Login" component={LoginTest} />
-
       <Tab.Screen
         name="Booking"
         component={Booking}
@@ -75,8 +60,6 @@ export default function BottomTabNavigator() {
           tabBarButton: (props) => <BookingButton {...props} />,
         }}
       />
-      <Tab.Screen name="LoadingScreen" component={LoadingScreen} />
-      <Tab.Screen name="Rooms" component={Rooms} />
       <Tab.Screen name="Contact" component={Contact} />
     </Tab.Navigator>
   );
--- pax-mobile/screens/Contact.jsx
+++ pax-mobile/screens/Contact.jsx
@@ -1,60 +1,60 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { useTheme } from '../theme/ThemeContext';
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { useTheme } from "../theme/ThemeContext";
 
 const Contact = () => {
   const { theme, isDark, toggleTheme } = useTheme();
-  const styles = createStyles (theme);
+  const styles = createStyles(theme);
 
   return (
-      <View style={styles.container}>
+    <View style={styles.container}>
       <Image
-      source={require("../assets/illustrationer/illustrationer_pax-01.svg")}
-      style={styles.image}
-      resizeMode="cover"
-    />
- <Text style={styles.textFet}>
-   Contact us:</Text>
-   <Text style={styles.text}>+46(0) 771-729 729 <br/> +46 (0) 771-PAX PAX
-    </Text>
-     </View>
-  )
+        source={require("../assets/illustrationer/illustrationer_pax-01.svg")}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <Text style={styles.textFet}>Contact us:</Text>
+      <Text style={styles.text}>
+        +46(0) 771-729 729 <br /> +46 (0) 771-PAX PAX
+      </Text>
+    </View>
+  );
 };
 
 export default Contact;
 
-const createStyles = (theme) => 
-    StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: theme.background,
-    width: "100vw",
-    minHeight: "100vh",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: { 
-    color: theme.textPrimary,
-    fontSize: 18, 
-    fontWeight: "400", 
-    textAlign: "center",
-    textTransform: "uppercase",
-    lineHeight: 30,
-    letterSpacing: 0.6,
-  },
-    textFet: { 
-    color: theme.textPrimary,
-    fontFamily: "Nunito",
-    fontSize: 18, 
-    fontWeight: "800", 
-    textAlign: "center",
-    textTransform: "uppercase",
-    lineHeight: 30,
-    letterSpacing: 0.6,
-  },
-  image: {
-    width: "90vw",
-    height: "35vh",
-  },
-});
+const createStyles = (theme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+      // width: "100vw",
+      // minHeight: "100vh",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    text: {
+      color: theme.textPrimary,
+      fontSize: 18,
+      fontWeight: "400",
+      textAlign: "center",
+      textTransform: "uppercase",
+      lineHeight: 30,
+      letterSpacing: 0.6,
+    },
+    textFet: {
+      color: theme.textPrimary,
+      fontFamily: "Nunito",
+      fontSize: 18,
+      fontWeight: "800",
+      textAlign: "center",
+      textTransform: "uppercase",
+      lineHeight: 30,
+      letterSpacing: 0.6,
+    },
+    image: {
+      // width: "90vw",
+      // height: "35vh",
+    },
+  });
